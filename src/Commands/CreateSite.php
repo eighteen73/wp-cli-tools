1495b59d8e1fe8966e0aebef41f7f63cbb95007a
Ed Jeavons <ed@orphans.co.uk>
2022-08-23 21:13:38 +0100

    Code readability

--- src/Commands/CreateSite.php
+++ src/Commands/CreateSite.php
@@ -15,7 +15,13 @@ use WP_CLI_Command;
  *
  * @package eighteen73/wpi-cli-tools
  */
-class CreateSite extends WP_CLI_Command {
+class CreateSite extends WP_CLI_Command
+{
+	private string $install_directory;
+	private string $site_name;
+	private string $site_url;
+	private string $site_username;
+	private string $site_password;
 
 	/**
 	 * Creates a new website.
@@ -32,56 +38,129 @@ class CreateSite extends WP_CLI_Command {
 	 *
 	 * @when before_wp_load
 	 */
-	public function create( array $args, array $assoc_args ) {
+	public function create(array $args, array $assoc_args)
+	{
 
-		// Get the installation name and path
-		$site_name = $args[0];
+		$this->check_path($args[0]);
+		$this->download_nebula();
+		$this->create_repo();
+		$this->download_pulsar();
+		$this->install_wordpress();
+
+		WP_CLI::line();
+		WP_CLI::line();
+		WP_CLI::success('Your website is ready.');
+		WP_CLI::line();
+		WP_CLI::line('URL:      ' . $this->site_url);
+		WP_CLI::line('Admin:    ' . $this->site_url . '/wp/wp-admin');
+		WP_CLI::line();
+		WP_CLI::line('Username: ' . $this->site_username);
+		WP_CLI::line('Password: ' . $this->site_password);
+	}
+
+	private function check_path(string $site_name)
+	{
 		if (preg_match('/^\//', $site_name)) {
-			$install_directory = rtrim($site_name, '/');
-			$site_name = basename($site_name);
+			$this->install_directory = rtrim($site_name, '/');
+			$this->site_name = basename($site_name);
 		} else {
-			$install_directory = rtrim(getcwd(), '/') . '/' . rtrim($site_name, '/');
+			$this->install_directory = rtrim(getcwd(), '/') . '/' . rtrim($site_name, '/');
+			$this->site_name = $site_name;
 		}
 
 		// Get confirmation
-		WP_CLI::confirm("Installing \"{$site_name}\" to \"{$install_directory}\". Is this OK?");
+		WP_CLI::confirm("Installing \"{$this->site_name}\" to \"{$this->install_directory}\". Is this OK?");
 
 		// Is it writable?
-		if ( ! is_dir( $install_directory ) ) {
-			if ( ! is_writable( dirname( $install_directory ) ) ) {
-				WP_CLI::error( "Insufficient permission to create directory '{$install_directory}'." );
+		if (!is_dir($this->install_directory)) {
+			if (!is_writable(dirname($this->install_directory))) {
+				WP_CLI::error("Insufficient permission to create directory '{$this->install_directory}'.");
 			}
 
-			WP_CLI::log( "Creating directory '{$install_directory}'." );
-			if ( ! @mkdir( $install_directory, 0777, true /*recursive*/ ) ) {
+			WP_CLI::log("Creating directory '{$this->install_directory}'.");
+			if (!@mkdir($this->install_directory, 0777, true /*recursive*/)) {
 				$error = error_get_last();
-				WP_CLI::error( "Failed to create directory '{$install_directory}': {$error['message']}." );
+				WP_CLI::error("Failed to create directory '{$this->install_directory}': {$error['message']}.");
 			}
 		}
-		if ( ! is_writable( $install_directory ) ) {
-			WP_CLI::error( "'{$install_directory}' is not writable by current user." );
+		if (!is_writable($this->install_directory)) {
+			WP_CLI::error("'{$this->install_directory}' is not writable by current user.");
 		}
 
-		// Use create the Nebula project
-		shell_exec('composer create-project --stability=dev eighteen73/nebula ' . escapeshellarg($install_directory) );
-		shell_exec('composer update --working-dir=' . escapeshellarg($install_directory) );
+	}
 
-		// Init the Git repo
-		exec('git -C ' . escapeshellarg($install_directory) . ' init', $output );
-		exec('git -C ' . escapeshellarg($install_directory) . ' add .', $output );
-		exec('git -C ' . escapeshellarg($install_directory) . ' commit -m "Initial commit"', $output );
+	private function download_nebula()
+	{
+		$this->run_command([
+			'composer',
+			'create-project',
+			[
+				'stability' => 'dev',
+			],
+			'eighteen73/nebula',
+			escapeshellarg($this->install_directory),
+		]);
+		$this->run_command([
+			'composer',
+			'update',
+			[
+				'quiet' => null,
+				'working-dir' => escapeshellarg($this->install_directory),
+			],
+		]);
+	}
 
-		// Add the Pulsar theme
-		exec('composer create-project --stability=dev eighteen73/pulsar ' . escapeshellarg($install_directory . '/web/app/themes/pulsar'), $output );
-		exec('git -C ' . escapeshellarg($install_directory) . ' add .', $output );
-		exec('git -C ' . escapeshellarg($install_directory) . ' commit -m "Add Pulsar theme"', $output );
+	private function create_repo()
+	{
+		$this->run_command([
+			'git',
+			'-C',
+			escapeshellarg($this->install_directory),
+			'init',
+		]);
+		$this->commit_repo('Initial commit');
+	}
 
-		// Install WordPress
-		$url = null;
-		$fp = @fopen( $install_directory . '/.env', 'r' );
+	private function commit_repo(string $message)
+	{
+		$this->run_command([
+			'git',
+			'-C',
+			escapeshellarg($this->install_directory),
+			'add',
+			'.',
+		]);
+		$this->run_command([
+			'git',
+			'-C',
+			escapeshellarg($this->install_directory),
+			'commit',
+			'-m',
+			escapeshellarg($message),
+		]);
+	}
+
+	private function download_pulsar()
+	{
+		$this->run_command([
+			'composer',
+			'create-project',
+			[
+				'quiet' => null,
+				'stability' => 'dev',
+			],
+			'eighteen73/pulsar',
+			escapeshellarg($this->install_directory . '/web/app/themes/pulsar'),
+		]);
+		$this->commit_repo('Add Pulsar theme');
+	}
+
+	private function install_wordpress()
+	{
+		$fp = @fopen($this->install_directory . '/.env', 'r');
 		while (($buffer = fgets($fp, 4096)) !== false) {
 			if (preg_match('/^WP_HOME="(.+)"$/', $buffer, $matches)) {
-				$url = $matches[1];
+				$this->site_url = $matches[1];
 				break;
 			}
 		}
@@ -90,32 +169,44 @@ class CreateSite extends WP_CLI_Command {
 		WP_CLI::line();
 		WP_CLI::line('Enter your admin username');
 		WP_CLI::out('> ');
-		$username = strtolower(trim(fgets(STDIN)));
+		$this->site_username = strtolower(trim(fgets(STDIN)));
 
 		WP_CLI::line();
 		WP_CLI::line('Enter your admin email address');
 		WP_CLI::out('> ');
 		$email = strtolower(trim(fgets(STDIN)));
 
-		exec( 'wp --path=' . escapeshellarg($install_directory . '/web/wp') . ' core install --skip-email --url=' . escapeshellarg($url . '/web') . ' --title=' . escapeshellarg($site_name ) . ' --admin_user=' . escapeshellarg($username ) . ' --admin_email=' . escapeshellarg($email ), $output );
-		exec( 'wp --path=' . escapeshellarg($install_directory . '/web/wp') . ' theme activate pulsar' );
+		// Maybe use https://make.wordpress.org/cli/handbook/references/internal-api/wp-cli-runcommand/
+		exec('wp --path=' . escapeshellarg($this->install_directory . '/web/wp') . ' core install --skip-email --url=' . escapeshellarg($this->site_url . '/web') . ' --title=' . escapeshellarg($this->site_name) . ' --admin_user=' . escapeshellarg($this->site_username) . ' --admin_email=' . escapeshellarg($email), $output);
+		exec('wp --path=' . escapeshellarg($this->install_directory . '/web/wp') . ' theme activate pulsar');
 
-		$password = '';
+		$this->site_password = '';
 		foreach ($output as $output_line) {
 			if (preg_match('/^Admin password: (.+)/', $output_line, $matches)) {
-				$password = trim($matches[1]);
+				$this->site_password = trim($matches[1]);
 				break;
 			}
 		}
+	}
 
-		WP_CLI::line();
-		WP_CLI::line();
-		WP_CLI::success( 'Your website is ready.' );
-		WP_CLI::line();
-		WP_CLI::line('URL:      ' . $url);
-		WP_CLI::line('Admin:    ' . $url . '/wp/wp-admin' );
-		WP_CLI::line();
-		WP_CLI::line('Username: ' . $username);
-		WP_CLI::line('Password: ' . $password);
+	private function run_command(array $command_parts)
+	{
+		$cmd = '';
+		foreach ($command_parts as $part) {
+			if (is_string($part)) {
+				$cmd .= " {$part}";
+				continue;
+			}
+			foreach ($part as $param => $param_value) {
+				if ($param_value === null) {
+					$cmd .= " --{$param}";
+				} else {
+					$cmd .= " --{$param}={$param_value}";
+				}
+			}
+		}
+		$cmd = trim($cmd);
+		exec($cmd, $output, $result);
+		return $output;
 	}
 }
