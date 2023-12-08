ba8feda54b6eb0ec6219055cacee2cb69ccacead
Ed Jeavons <ed@orphans.co.uk>
2023-12-08 11:15:54 +0000

    Rephrase "sample content" as "style guide"

--- src/Commands/StyleGuide.php
+++ src/Commands/StyleGuide.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Add predetermined sample content to the website.
+ * Add a predetermined style guide to the website.
  *
  * @package eighteen73/wpi-cli-tools
  */
@@ -8,26 +8,24 @@
 namespace Eighteen73\WP_CLI\Commands;
 
 use Eighteen73\WP_CLI\Helpers;
-use WP_Block_Pattern_Categories_Registry;
 use WP_Block_Patterns_Registry;
 use WP_CLI;
-use WP_CLI\ExitException;
 use WP_CLI_Command;
 
 /**
- * Add predetermined sample content to the website.
+ * Add a predetermined style guide to the website.
  *
  * ## EXAMPLES
  *
- *     # Add sample content to the website for testing
- *     $ wp eighteen73 sample-content
+ *     # Add a style guide to the website for testing/sign-off
+ *     $ wp eighteen73 style-guide
  *
  * @package eighteen73/wpi-cli-tools
  */
-class SampleContent extends WP_CLI_Command {
+class StyleGuide extends WP_CLI_Command {
 
 	/**
-	 * Add predetermined sample content to the website.
+	 * Add a predetermined style guide to the website.
 	 *
 	 * Pages are published privately. You'll be asked for permission to overwrite pre-existing pages.
 	 *
@@ -40,7 +38,7 @@ class SampleContent extends WP_CLI_Command {
 	 *
 	 * ## EXAMPLES
 	 *
-	 *     wp eighteen73 sample-content
+	 *     wp eighteen73 style-guide
 	 *
 	 * @when after_wp_load
 	 *
@@ -55,16 +53,16 @@ class SampleContent extends WP_CLI_Command {
 
 		$parent_page_id = $this->prepare_parent_page();
 
-		$sample_files = $this->get_sample_files();
-		foreach ( $sample_files as $sample_file ) {
-			$page_id = $this->get_sample_page( $sample_file['slug'] );
+		$content_files = $this->get_style_guide_content();
+		foreach ( $content_files as $content_file ) {
+			$page_id = $this->get_style_guide_page( $content_file['slug'] );
 
 			// Skip if we don't have a page (i.e. we aren't overwriting existing)
 			if ( ! $page_id ) {
 				continue;
 			}
 
-			$this->update_sample_page( $parent_page_id, $page_id, $sample_file['title'], $sample_file['path'], $sample_file['special_insert'] );
+			$this->update_style_guide_page( $parent_page_id, $page_id, $content_file['title'], $content_file['path'], $content_file['special_insert'] );
 		}
 
 		$this->add_links_to_parent_page( $parent_page_id );
@@ -89,13 +87,13 @@ class SampleContent extends WP_CLI_Command {
 	}
 
 	/**
-	 * Get a list of the sample files that are available in plugin
+	 * Get a list of the style guide files that are available in this package
 	 *
 	 * @return array
 	 */
-	protected function get_sample_files(): array {
+	protected function get_style_guide_content(): array {
 		$out     = [];
-		$dirname = dirname( __DIR__, 2 ) . '/sample-content';
+		$dirname = dirname( __DIR__, 2 ) . '/style-guide';
 		$files   = array_filter( scandir( $dirname ), fn ( $filename ) => str_ends_with( $filename, '.html' ) );
 		foreach ( $files as $file ) {
 
@@ -127,12 +125,12 @@ class SampleContent extends WP_CLI_Command {
 	}
 
 	/**
-	 * Load or create the sample page
+	 * Load or create the style guide page
 	 *
 	 * @param string $path The page path
 	 * @return int
 	 */
-	protected function get_sample_page( string $path ): ?int {
+	protected function get_style_guide_page( string $path ): ?int {
 
 		// Try existing
 		$args  = [
@@ -165,26 +163,26 @@ class SampleContent extends WP_CLI_Command {
 	}
 
 	/**
-	 * Update the content of a sample page.
+	 * Update the content of a style guide page.
 	 *
-	 * Given a page ID and a path to a sample file, this method updates the content
-	 * of the specified page with the contents of the sample file.
+	 * Given a page ID and a path to a style guide template file, this method updates the content
+	 * of the specified page with the contents of the style guide template file.
 	 *
 	 * @param int     $parent_page_id The ID of the page parent.
 	 * @param int     $page_id The ID of the page to update.
 	 * @param string  $title The title of the page to update.
-	 * @param string  $sample_filepath The path to the sample file.
-	 * @param ?string $special_insert The sample's requirement for any special inserted content.
+	 * @param string  $style_guide_content_filepath The path to the style guide template file.
+	 * @param ?string $special_insert The style guide's requirement for any special inserted content.
 	 *
 	 * @return void
 	 */
-	protected function update_sample_page( int $parent_page_id, int $page_id, string $title, string $sample_filepath, string $special_insert = null ): void {
+	protected function update_style_guide_page( int $parent_page_id, int $page_id, string $title, string $style_guide_content_filepath, string $special_insert = null ): void {
 		$arg = [
 			'post_parent'  => $parent_page_id,
 			'ID'           => $page_id,
 			'post_status'  => 'private',
 			'post_title'   => $title,
-			'post_content' => file_get_contents( $sample_filepath ),
+			'post_content' => file_get_contents( $style_guide_content_filepath ),
 		];
 
 		if ( $special_insert === 'theme_patterns' ) {
@@ -200,7 +198,7 @@ class SampleContent extends WP_CLI_Command {
 	 * @return int The ID of the parent page
 	 */
 	protected function prepare_parent_page(): int {
-		$parent_page_id = $this->get_sample_page( 'styleguide' );
+		$parent_page_id = $this->get_style_guide_page( 'styleguide' );
 
 		$arg = [
 			'ID'          => $parent_page_id,
@@ -228,7 +226,6 @@ class SampleContent extends WP_CLI_Command {
 				'post_status' => [ 'publish', 'private' ],
 			]
 		);
-		// dump($pages);
 
 		$content  = '<!-- wp:paragraph {"className":""} -->';
 		$content .= '<p>This page and it\'s children serve the purpose of allowing the website\'s team to validate the website\'s styles and content blocks. They are automatically generated so if you edit them please be aware that the changes may be overwritten in the future. </p>';
